1623a9a2abb83cde59ebe2ffa2d6428a1afdd7ac
Rizu0007 <87184120+Rizu0007@users.noreply.github.com>
2023-04-27 23:20:59 +0500

    f

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ export default function Home() {
   return (
    
      <div>
-     <h1 className='text-bold text-red-500npx '> Hello</h1>
+     <h1 className='text-bold text-red-500npx '>  g 
+     Hello</h1>
      </div>
   )
 }
