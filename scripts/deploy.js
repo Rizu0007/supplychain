96ed3d5d3c0e4c60873fedcc7efaa5acb2d0628b
Rizu0007 <87184120+Rizu0007@users.noreply.github.com>
2023-08-19 20:37:34 +0500

    navbar

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,18 @@
-
 const hre = require("hardhat");
 
 async function main() {
-  const Tracking = await hre.ethers.deployContract("Track");
+
+  const Tracking = await hre.ethers.deployContract("Tracking");
 
   await Tracking.waitForDeployment();
 
-
   console.log(
-   `Tracking the   ${Tracking.target}`
+    `Tracking deployed to ${Tracking.target}`
   );
 }
 
-
-
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
